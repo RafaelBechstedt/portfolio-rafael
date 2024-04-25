184179e25a1ca1fab3b9d032d9be7a0bc23d6910
Rafael Bechstedt <bechstedt@hotmail.com>
2024-04-25 19:31:27 -0300

    Ajusta tecnologias e asobre mim

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,19 +3,20 @@
 export default function AboutContainer() {
   return (
     <section className="about-container">
-    <h2>Sobre</h2>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, dolores!
-      Repellendus eius consequuntur, voluptate deserunt perferendis mollitia
-      debitis est autem aperiam necessitatibus beatae assumenda illo rem
-      architecto quis dolor quae.
-    </p>
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos veritatis
-      aperiam, odit nihil placeat, dolorum sequi reprehenderit fugiat,
-      distinctio provident impedit modi neque sapiente voluptate voluptates.
-      Maiores sint at dolores?
-    </p>
-  </section>
+      <h2>Sobre</h2>
+      <p>
+        Sou um desenvolvedor web full stack, com formação prévia
+        em engenharia civil, onde pude desenvolver um forte
+        raciocínio lógico matemático, lidando com problemas
+        matemáticos complexos. Possuo inglês fluente, que
+        aperfeiçoei durante minha estadia de 3 anos na Austrália,
+        desenvolvendo e pondo em prática inglês em ambiente de
+        trabalho. Dediquei-me intensamente ao estudo da programação 
+        web na Trybe, onde foram aproximadamente 1.500 horas de conteúdo, 
+        com dezenas de projetos prátricos. Hoje trabalho como desenvolvedor freelancer,
+        desenvolvendo aplicações webs e scripts de automações em
+        geral.
+      </p>
+    </section>
   )
 }
--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,21 +1,22 @@
 import {
-    DiHtml5,
-    DiCss3,
-    DiJsBadge,
+    DiDocker,
     DiNodejsSmall,
     DiMysql,
     DiReact,
+    DiJava,
 } from "react-icons/di";
+
+import {SiTypescript} from "react-icons/si"
 
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
-    { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-    { id: "css", name: "CSS3", icon: <DiCss3 /> },
-    { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-    { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-    { id: "mysql", name: "MySQL", icon: <DiMysql /> },
     { id: "react", name: "React", icon: <DiReact /> },
+    { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+    { id: "ts", name: "TypeScript", icon: <SiTypescript /> },
+    { id: "docker", name: "Docker", icon: <DiDocker /> },
+    { id: "java", name: "Java", icon: <DiJava /> },
+    { id: "mysql", name: "MySQL", icon: <DiMysql /> },
   ];
 
 export default function TechnologiesContainer() {
@@ -28,7 +29,6 @@ export default function TechnologiesContainer() {
               {tech.icon}
               <div className="technology-info">
                 <h3>{tech.name}</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
               </div>
             </div>
           ))}
