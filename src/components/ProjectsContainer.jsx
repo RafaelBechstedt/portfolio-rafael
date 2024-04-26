3f918fe5fd36f8e999675824a6c23ccaa70536c4
Rafael Bechstedt <bechstedt@hotmail.com>
2024-04-26 00:42:25 -0300

    Altera um pouco estilização do main container na parte dos projetos

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,18 +1,32 @@
-
+import '../styles/components/maincontent.sass'
 
 export default function ProjectsContainer() {
   return (
-    <section className="projects-container">
-    <h2>Projetos</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore at
-      rem fugit provident dolorum exercitationem, ducimus quisquam ratione
-      cumque reiciendis accusamus totam, veniam iusto numquam distinctio.
-      Dignissimos labore aspernatur pariatur?
-    </p>
-    <a href="#" className="btn">
-      Ver Projetos
-    </a>
-  </section>
+    <>
+      <h2>Projetos</h2>
+      <section className="projects-container">
+        <h3>Front-End</h3>
+        <section className="projects-front">
+          <a href="https://rafaelbechstedt.github.io/app-de-receitas-react/" className="btn" target="_blank">
+            App de receitas
+          </a>
+          <a href="https://rafaelbechstedt.github.io/loja-online-front/" className="btn" target="_blank">
+            Loja online
+          </a>
+        </section>
+        <h3>Back-End</h3>
+        <section className="projects-back">
+          <a href="https://github.com/RafaelBechstedt/store-manager-API-REST-MSC" className="btn" target="_blank">
+            Store manager API
+          </a>
+          <a href="https://github.com/RafaelBechstedt/blogs-api-node.js-sequelize" className="btn" target="_blank">
+            Blogs API
+          </a>
+          <a href="https://github.com/RafaelBechstedt/trybers-and-dragons-TS-backend" className="btn" target="_blank">
+            Trybers and Dragons
+          </a>
+        </section>
+      </section>
+    </>
   )
 }
